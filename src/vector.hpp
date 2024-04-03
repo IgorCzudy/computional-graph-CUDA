0a0810d4692bbf7602fcea40ebcb568eda6fab64
Anna Panfil <anna.panfil@student.put.poznan.pl>
2024-04-03 11:57:01 +0200

    feat: Add tests for existing and feature functions

--- src/vector.hpp
+++ src/vector.hpp
@@ -17,9 +17,16 @@ public:
 
 
 public:
+    Vector() {}  // Default constructor for initialization
     Vector(std::vector<float> values_int, std::string label_int);
     Vector operator+(Vector& other);
     Vector operator*(Vector& other);
+    Vector operator*(float other) {throw("Not implemented"); return *this;} // Not implemented;
+    Vector operator-(Vector& other) {throw ("Not implemented"); return *this;};
+    Vector operator-() {throw ("Not implemented"); return *this;};
+    Vector operator/(Vector& other) {throw ("Not implemented"); return *this;};
+    Vector operator^(float power) {throw ("Not implemented"); return *this;};
+
     void backword();
     // void setName(std::string n);
     // void setAge(int a);
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,34 +1,92 @@
 #include <gtest/gtest.h>
 #include "../src/vector.hpp"
 
+struct NewtorchTest : public testing::Test {
+    void SetUp() override{
+        vec1 = Vector({2.0, 3.0, 4.0}, "vec1");
+        vec2 = Vector({4.0, 5.0, 6.0}, "vec2");
+    }
 
-TEST(SquareRootTest, PositiveNos) { 
-    std::vector<float> elements = {2.0};//, 2.0, 3.0};
-    Vector vec1(elements, "vec1");
+    void compare_vectors(std::vector<float> v, std::vector<float> expected){
+        ASSERT_EQ(v.size(), expected.size()) << "Vectors x and y are of unequal length";
+
+        for (int i = 0; i < v.size(); ++i) {
+            EXPECT_EQ(v[i], expected[i]) << "Vectors x and y differ at index " << i;
+        }
+
+    }
     
-    std::vector<float> elements2 = {4.0};//, 5.0, 6.0};
-    Vector vec2(elements2, "vec2");
+    Vector vec1;
+    Vector vec2;
+};
 
-    Vector vec3 = vec1 * vec2;
-    vec3.backword();
-
-    ASSERT_EQ(4, vec1.grads[0]);
-    ASSERT_EQ(2, vec2.grads[0]);
-    ASSERT_EQ(1, vec3.grads[0]);
-}
-
-
-TEST(AdditionTest, PositiveNos) { 
-    std::vector<float> elements = {2.0};//, 2.0, 3.0};
-    Vector vec1(elements, "vec1");
-    std::vector<float> elements2 = {4.0};//, 5.0, 6.0};
-    Vector vec2(elements2, "vec2");
+TEST_F(NewtorchTest, AdditionTest) {
     Vector vec3 = vec1 + vec2;
     vec3.backword();
 
-    ASSERT_EQ(1, vec1.grads[0]);
-    ASSERT_EQ(1, vec2.grads[0]);
-    ASSERT_EQ(1, vec3.grads[0]);
+    compare_vectors(vec1.grads, {1, 1, 1});
+    compare_vectors(vec2.grads, {1, 1, 1});
+    compare_vectors(vec3.grads, {1, 1, 1});
+    compare_vectors(vec3.values, {6, 8, 10});
+}
+
+TEST_F(NewtorchTest, MultiplicationTest) { 
+    Vector vec3 = vec1 * vec2;
+    vec3.backword();
+
+    compare_vectors(vec1.grads, {4, 5, 6});
+    compare_vectors(vec2.grads, {2, 3, 4});
+    compare_vectors(vec3.grads, {1, 1, 1});
+    compare_vectors(vec3.values, {8, 15, 24});
+}
+
+TEST_F(NewtorchTest, MultiplicationByFloatTest){
+    Vector vec3 = vec1 * 2;
+    vec3.backword();
+
+    compare_vectors(vec1.grads, {2, 2, 2});
+    compare_vectors(vec3.grads, {1, 1, 1});
+    compare_vectors(vec3.values, {4, 6, 8});
+
+}
+
+TEST_F(NewtorchTest, PowerTest) { 
+    Vector vec3 = vec1 ^ 3;
+    vec3.backword();
+
+    compare_vectors(vec1.grads, {8, 18, 32}); //2 * vec1 * vec1
+    compare_vectors(vec3.grads, {1, 1, 1});
+    compare_vectors(vec3.values, {8, 27, 64});
+}
+
+TEST_F(NewtorchTest, NegationTest){
+    Vector vec3 = -vec1;
+    vec3.backword();
+
+    compare_vectors(vec1.grads, {-1, -1, -1});
+    compare_vectors(vec3.grads, {1, 1, 1});
+    compare_vectors(vec3.values, {-2, -3, -4});
+
+}
+
+TEST_F(NewtorchTest, DivisionTest) { 
+    Vector vec3 = vec1 / vec2;
+    vec3.backword();
+
+    compare_vectors(vec1.grads, (vec2^(-1)).values);
+    compare_vectors(vec2.grads, (-vec2 * vec1).values);
+    compare_vectors(vec3.grads, {1, 1, 1});
+    compare_vectors(vec3.values, {0.5, 3/5, 4/6});
+}
+
+TEST_F(NewtorchTest, SubtractionTest) { 
+    Vector vec3 = vec1 - vec2;
+    vec3.backword();
+
+    compare_vectors(vec1.grads, {1, 1, 1});
+    compare_vectors(vec2.grads, {-1, -1, -1});
+    compare_vectors(vec3.grads, {1, 1, 1});
+    compare_vectors(vec3.values, {-2, -2, -2});
 }
 
 int main(int argc, char **argv) {
