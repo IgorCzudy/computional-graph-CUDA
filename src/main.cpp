429f841433672f2d7e01e4dbf2ad86e0321de627
Anna Panfil <anna.panfil@student.put.poznan.pl>
2024-05-06 15:37:32 +0200

    feat: change all operations to shared ptrs

--- src/main.cpp
+++ src/main.cpp
@@ -7,49 +7,20 @@ int main() {
     try{
         std::shared_ptr<Vector> vector1 = std::make_shared<Vector>(std::vector<float>{2.0}, "vec1");
         std::shared_ptr<Vector> vector2 = std::make_shared<Vector>(std::vector<float>{4.0}, "vec2");
-        std::shared_ptr<Vector> vector3 = std::make_shared<Vector>(std::vector<float>{3.0}, "vec3");
-        std::shared_ptr<Vector> vector4 = vector1 * vector2 * vector3;
-        std::cout << vector4->values[0] << std::endl;
+        std::shared_ptr<Vector> vector3 = std::make_shared<Vector>(std::vector<float>{-1.0}, "vec3");
+
+        std::shared_ptr<Vector> vector4 = vector2 / vector1;
+        
+        std::cout << "result " << vector4->values[0] << std::endl;
         vector4->backword();
-        std::cout << vector1->grads[0] << std::endl;
-        std::cout << vector2->grads[0] << std::endl;
-        std::cout << vector3->grads[0] << std::endl;
-
-        // Vector vec1({2.0}, "vec1");
-        // Vector vec2({4.0}, "vec2");
-
-        // Vector vec4({3.0}, "vec4");
-        // std::cout << (vec1 * vec2 * vec4).values[0] << std::endl;
-        // Vector vec5 = vec1 * vec2 * vec4;
-        // std::cout << vec5.values[0] << std::endl;
-        // vec5.backword();
-        // std::cout << vec1.grads[0] << std::endl;
-        
-        // Vector vec8 = -vec1;
-        // vec8.backword();
-        // std::cout << vec8.values[0] << std::endl;
-        // std::cout << vec1.grads[0] << std::endl;
-
-        // Vector vec3 = vec1 * vec2; //8
-        // vec3.backword();
-        
-        // Vector vec5 = vec3 * vec4; //24
-        // vec5.backword();
-
-        // Vector vec6 = vec5 * 2; //48
-        // vec6.backword();
-
-        // Vector vec7 = vec6 ^ 3; 
-        // vec7.backword();
+        std::cout << "grad1 " << vector1->grads[0] << std::endl;
+        std::cout << "grad2 " << vector2->grads[0] << std::endl;
+        std::cout << "grad3 " << vector3->grads[0] << std::endl;
+        std::cout << "grad result " << vector4->grads[0] << std::endl;
     }
     catch(const char* msg){
         std::cout << "ERROR: " << msg << std::endl;
     }
-
-
-    // std::cout <<"vec1: "<<vec1.grads[0]<<" "<< vec1.grads[1]<<" "<< vec1.grads[2] << std::endl; 
-    // std::cout <<"vec2: "<<vec2.grads[0]<<" "<< vec2.grads[1]<<" "<< vec2.grads[2] << std::endl; 
-    // std::cout <<"vec3: "<<vec3.grads[0]<<" "<< vec3.grads[1]<<" "<< vec3.grads[2] << std::endl; 
 
     return 0;
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -3,103 +3,115 @@
 
 struct NewtorchTest : public testing::Test {
     void SetUp() override{
-        vec1 = Vector({2.0, 3.0, 4.0}, "vec1");
-        vec2 = Vector({4.0, 5.0, 6.0}, "vec2");
+        vec1 = std::make_shared<Vector>(std::vector<float>{2.0, 3.0, 4.0}, "vec1");
+        vec2 = std::make_shared<Vector>(std::vector<float>{4.0, 5.0, 6.0}, "vec2");
     }
 
-    void compare_vectors(std::vector<float> v, std::vector<float> expected){
-        ASSERT_EQ(v.size(), expected.size()) << "Vectors x and y are of unequal length";
+    void compare_vectors(std::vector<float> v, std::vector<float> expected, std::string name){
+        ASSERT_EQ(v.size(), expected.size()) << name <<" are of unequal length";
 
         for (int i = 0; i < v.size(); ++i) {
-            EXPECT_EQ(v[i], expected[i]) << "Vectors x and y differ at index " << i;
+            EXPECT_EQ(v[i], expected[i]) << name << " differ at index " << i;
         }
 
     }
     
-    Vector vec1;
-    Vector vec2;
+    std::shared_ptr<Vector> vec1;
+    std::shared_ptr<Vector> vec2;
 };
 
 TEST_F(NewtorchTest, AdditionTest) {
-    Vector vec3 = vec1 + vec2;
-    vec3.backword();
+    std::shared_ptr<Vector> vec3 = vec1 + vec2;
+    vec3->backword();
 
-    compare_vectors(vec1.grads, {1, 1, 1});
-    compare_vectors(vec2.grads, {1, 1, 1});
-    compare_vectors(vec3.grads, {1, 1, 1});
-    compare_vectors(vec3.values, {6, 8, 10});
+    compare_vectors(vec1->grads, {1, 1, 1}, "gradients of vec1");
+    compare_vectors(vec2->grads, {1, 1, 1}, "gradients of vec2");
+    compare_vectors(vec3->grads, {1, 1, 1}, "gradients of vec3");
+    compare_vectors(vec3->values, {6, 8, 10}, "results");
 }
 
 TEST_F(NewtorchTest, ThreeNumbersAdditionTest){
-    Vector vec3({1.0, 2.0, 3.0}, "vec3");
-    Vector vec4 = vec1 + vec2 + vec4;
-    vec4.backword();
+    std::shared_ptr<Vector> vec3 = std::make_shared<Vector>(std::vector<float>{1.0, 2.0, 3.0}, "vec3");
+    std::shared_ptr<Vector> vec4 = vec1 + vec2 + vec3;
+    vec4->backword();
 
-    compare_vectors(vec1.grads, {1, 1, 1});
-    compare_vectors(vec2.grads, {1, 1, 1});
-    compare_vectors(vec3.grads, {1, 1, 1});
-    compare_vectors(vec4.grads, {1, 1, 1});
-    compare_vectors(vec4.values, {7, 10, 13});
+    compare_vectors(vec1->grads, {1, 1, 1}, "gradients of vec1");
+    compare_vectors(vec2->grads, {1, 1, 1}, "gradients of vec2");
+    compare_vectors(vec3->grads, {1, 1, 1}, "gradients of vec3");
+    compare_vectors(vec4->grads, {1, 1, 1}, "gradients of vec4");
+    compare_vectors(vec4->values, {7, 10, 13}, "results");
+}
+
+TEST_F(NewtorchTest, MultiplicationTest) { 
+    std::shared_ptr<Vector> vec3 = vec1 * vec2;
+    vec3->backword();
+
+    compare_vectors(vec1->grads, {4, 5, 6}, "gradients of vec1");
+    compare_vectors(vec2->grads, {2, 3, 4}, "gradients of vec2");
+    compare_vectors(vec3->grads, {1, 1, 1}, "gradients of vec1");
+    compare_vectors(vec3->values, {8, 15, 24}, "results");
+}
+
+TEST_F(NewtorchTest, ThreeNumbersMultiplicationTest){
+    std::shared_ptr<Vector> vec3 = std::make_shared<Vector>(std::vector<float>{1.0, 2.0, 3.0}, "vec3");
+    std::shared_ptr<Vector> vec4 = vec1 * vec2 * vec3;
+    vec4->backword();
+
+    compare_vectors(vec1->grads, {4, 10, 18}, "gradients of vec1");
+    compare_vectors(vec2->grads, {2, 6, 12}, "gradients of vec2");
+    compare_vectors(vec3->grads, {8, 15, 24}, "gradients of vec3");
+    compare_vectors(vec4->grads, {1, 1, 1}, "gradients of vec4");
+    compare_vectors(vec4->values, {8, 30, 72}, "results");
 }
     
 
-TEST_F(NewtorchTest, MultiplicationTest) { 
-    Vector vec3 = vec1 * vec2;
-    vec3.backword();
-
-    compare_vectors(vec1.grads, {4, 5, 6});
-    compare_vectors(vec2.grads, {2, 3, 4});
-    compare_vectors(vec3.grads, {1, 1, 1});
-    compare_vectors(vec3.values, {8, 15, 24});
-}
-
 TEST_F(NewtorchTest, MultiplicationByFloatTest){
-    Vector vec3 = vec1 * 2;
-    vec3.backword();
+    std::shared_ptr<Vector> vec3 = vec1 * 2;
+    vec3->backword();
 
-    compare_vectors(vec1.grads, {2, 2, 2});
-    compare_vectors(vec3.grads, {1, 1, 1});
-    compare_vectors(vec3.values, {4, 6, 8});
+    compare_vectors(vec1->grads, {2, 2, 2}, "gradients of vec1");
+    compare_vectors(vec3->grads, {1, 1, 1}, "gradients of vec3");
+    compare_vectors(vec3->values, {4, 6, 8}, "results");
 
 }
 
 TEST_F(NewtorchTest, PowerTest) { 
-    Vector vec3 = vec1 ^ 3;
-    vec3.backword();
+    std::shared_ptr<Vector> vec3 = vec1 ^ 3;
+    vec3->backword();
 
-    compare_vectors(vec1.grads, {12, 27, 48}); //3 * vec1 * vec1
-    compare_vectors(vec3.grads, {1, 1, 1});
-    compare_vectors(vec3.values, {8, 27, 64});
+    compare_vectors(vec1->grads, {12, 27, 48}, "gradients of vec1"); //3 * vec1 * vec1
+    compare_vectors(vec3->grads, {1, 1, 1}, "gradients of vec3");
+    compare_vectors(vec3->values, {8, 27, 64}, "results");
 }
 
 TEST_F(NewtorchTest, NegationTest){
-    Vector vec3 = -vec1;
-    vec3.backword();
+    std::shared_ptr<Vector> vec3 = -vec1;
+    vec3->backword();
 
-    compare_vectors(vec1.grads, {-1, -1, -1});
-    compare_vectors(vec3.grads, {1, 1, 1});
-    compare_vectors(vec3.values, {-2, -3, -4});
+    compare_vectors(vec1->grads, {-1, -1, -1}, "gradients of vec1");
+    compare_vectors(vec3->grads, {1, 1, 1}, "gradients of vec3");
+    compare_vectors(vec3->values, {-2, -3, -4}, "results");
 
 }
 
 TEST_F(NewtorchTest, DivisionTest) { 
-    Vector vec3 = vec1 / vec2;
-    vec3.backword();
+    std::shared_ptr<Vector> vec3 = vec1 / vec2;
+    vec3->backword();
 
-    compare_vectors(vec1.grads, (vec2^(-1)).values);
-    compare_vectors(vec2.grads, (-vec2 * vec1).values);
-    compare_vectors(vec3.grads, {1, 1, 1});
-    compare_vectors(vec3.values, {0.5, 3/5, 4/6});
+    compare_vectors(vec1->grads, {1.0/4, 1.0/5, 1.0/6}, "gradients of vec1"); //(vec2^(-1))->values
+    compare_vectors(vec2->grads, {-2.0/16, -3.0/25, -4.0/36}, "gradients of vec2");//(-(vec2^(-2)) * vec1)->values
+    compare_vectors(vec3->grads, {1, 1, 1}, "gradients of vec3");
+    compare_vectors(vec3->values, {1.0/2, 3.0/5, 4.0/6}, "results");
 }
 
 TEST_F(NewtorchTest, SubtractionTest) { 
-    Vector vec3 = vec1 - vec2;
-    vec3.backword();
+    std::shared_ptr<Vector> vec3 = vec1 - vec2;
+    vec3->backword();
 
-    compare_vectors(vec1.grads, {1, 1, 1});
-    compare_vectors(vec2.grads, {-1, -1, -1});
-    compare_vectors(vec3.grads, {1, 1, 1});
-    compare_vectors(vec3.values, {-2, -2, -2});
+    compare_vectors(vec1->grads, {1, 1, 1}, "gradients of vec1");
+    compare_vectors(vec2->grads, {-1, -1, -1}, "gradients of vec2");
+    compare_vectors(vec3->grads, {1, 1, 1}, "gradients of vec3");
+    compare_vectors(vec3->values, {-2, -2, -2}, "results");
 }
 
 int main(int argc, char **argv) {
